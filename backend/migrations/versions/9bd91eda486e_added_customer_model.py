e518082c41bc5abcc98e23273b7cac84df258646
Devin <devincb93@gmail.com>
2024-08-24 13:40:24 -0700

    Added model and validations for customer and notifications

---
+++ backend/migrations/versions/9bd91eda486e_added_customer_model.py
@@ -0,0 +1,24 @@
+"""Added Customer model
+
+Revision ID: 9bd91eda486e
+Revises: 5e1f70d1dddd
+Create Date: 2024-08-24 13:37:08.865335
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9bd91eda486e'
+down_revision = '5e1f70d1dddd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- backend/models.py
+++ backend/models.py
@@ -25,3 +25,40 @@ class Employee(db.Model, SerializerMixin):
             raise ValueError("Password didn't meet character criteria")
         else:
             return password
+        
+class Customer(db.Model, SerializerMixin):
+    __tablename__='customers'
+
+    id = Column(Integer, primary_key=True)
+    first_last_name = Column(String, nullable=False)
+    phone_number = Column(Integer, min=10, max=10)
+    email = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+    @validates('first_last_name')
+    def validate_names(self, key, first_lase_name):
+        if len(first_lase_name) < 0:
+            raise ValueError("Must enter a name to add to queue")
+        else:
+            return first_lase_name
+    @validates('phone_number')
+    def validate_number(self, key, phone_number):
+        if len(phone_number) < 10:
+            raise ValueError("Must enter a valid phone number")
+        else:
+            return phone_number
+        
+class Notification(db.Model, SerializerMixin):
+    __tablename__='notifications'
+
+    id = Column(Integer, primary_key=True)
+    message = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+    @validates('message')
+    def validate_message(self, key, message):
+        if len(message) < 0:
+            raise ValueError('No message added for notification, please add one')
+        else:
+            return message
